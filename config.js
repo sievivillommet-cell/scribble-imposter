1f841a4b9d2edec8c852022cffa7f998b6adfcd6
sievivillommet-cell <sievi.villommet@gmail.com>
2026-08-29 23:02:22 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // 2) Project URL und Publishable Key hier eintragen.
 //    Der Publishable/anon Key darf in einer Browser-App stehen. NIEMALS den service_role Key verwenden.
 window.SCRIBBLE_CONFIG = {
-  SUPABASE_URL: 'https://onraqlasxildkrrbhvru.supabase.co/rest/v1/',
+  SUPABASE_URL: 'https://onraqlasxildkrrbhvru.supabase.co',
   SUPABASE_KEY: 'sb_publishable__zDZU2fuSSdvqFryfyXUwg_YaWb95Mn'
 };
